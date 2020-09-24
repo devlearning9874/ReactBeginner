0db9c283ee94ff0e8791f8bd0f72824562fec353
devlearning9874 <67912369+devlearning9874@users.noreply.github.com>
2020-09-24 16:26:58 +0530

    Render variables

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
-import React from "react"
-import ReactDom from "react-dom"
+import React from "react";
+import ReactDom from "react-dom";
 
+const List = ["Milk", "Butter", "Cheese", "Souce"];
 ReactDom.render(
-<div>
-<h1>Purchase List</h1>
-<ul>
-<li>Milk </li>
-<li>Milk</li>
-<li>ilk</li>
-<li>Silk</li>
-  </ul>
-</div>,
-document.getElementById("root")
+  <div>
+    <h1>Purchase List</h1>
+    <ul>
+      <li>{List[0]} </li>
+      <li>{List[1]}</li>
+      <li>{List[2]}</li>
+      <li>{List[3]}</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
 );
